9b565916d1744a0c4cbe685816194339df4e0f58
Erno Palonheimo <esp@iki.fi>
2016-01-27 10:25:54 +0200

    now it actually does something

--- classes/App.php
+++ classes/App.php
@@ -2,10 +2,16 @@
 
 class App
 {
-
   public function run()
   {
-    echo "hei\n";
-  }
+    try {
+      $server = new SoapServer(NULL, array('uri' => 'http://finna-proxy.anteek.fi/'));
+      
+      $server->setClass('Api');
+      $server->handle();
 
+    } catch (SOAPFault $f) {
+      Log::write($f->faultstring);
+    }
+  }
 }
